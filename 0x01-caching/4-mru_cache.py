00dcc6b09605bffc3d26dea6511c8a468915a0eb
posh <mrposhboo@gmail.com>
2024-05-09 00:29:37 +0100

    787

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
-"""Defines a MRUCache class that inherits from BaseCaching"""
+"""AAA"""
 
-BaseCaching = __import__('base_caching').BaseCaching
-
+from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
-    """A caching system using MRU algorithm"""
-
+    """AAA"""
     def __init__(self):
-        """Initialize the MRUCache instance"""
+        """AAA"""
         super().__init__()
+        self.stack = []
 
     def put(self, key, item):
-        """Assigns item to key in self.cache_data"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # If cache is full, discard the most recently used item
-                mru_key = next(reversed(self.cache_data))
-                del self.cache_data[mru_key]
-                print("DISCARD:", mru_key)
+        """AAA"""
+        if key and item:
+            if self.cache_data.get(key):
+                self.stack.remove(key)
+            while len(self.stack) >= self.MAX_ITEMS:
+                delete = self.stack.pop()
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+            self.stack.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """Returns the value linked to key in self.cache_data"""
-        if key is not None:
-            if key in self.cache_data:
-                # If key exists, move it to the end of the cache data
-                value = self.cache_data.pop(key)
-                self.cache_data[key] = value
-                return value
-        return None
+        """AAA"""
+        if self.cache_data.get(key):
+            self.stack.remove(key)
+            self.stack.append(key)
+        return self.cache_data.get(key)
